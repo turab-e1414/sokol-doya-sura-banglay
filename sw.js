786003aee539fcf7abf045f9b8e2d6d40a3af091
turab-e1414 <didarulislamturab@gmail.com>
2026-02-27 02:42:29 +0600

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,22 +1,33 @@
-const cacheName = 'prayer-app-v1';
+const CACHE_NAME = 'prayer-app-v2';
 const assets = [
   './',
   './index.html',
   'https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;600&family=Amiri&display=swap'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
       return cache.addAll(assets);
     })
   );
+  self.skipWaiting();
 });
-ো
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => caches.match('./index.html'))
   );
 });
